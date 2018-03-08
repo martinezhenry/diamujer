3f0b79009487f5898897f3c1d70519e521c6188e
Henry Martinez <hmartinez@novopayment.com>
2018-03-08 13:48:50 -0500

    upgrade 1

--- main.php
+++ main.php
@@ -25,7 +25,7 @@ and open the template in the editor.
         <div class="title">
             <label>Fel&iacute;z D&iacute;a</label>
         </div>
-        
+        <br>
         <div class="mainBox">
             <label>Por ser fuente de alegr&iacute;as, inspiraci&oacute;n y amor, por esa lucha que das d&iacute;a tras d&iacute;a como si no existiera un ma&ntilde;ana, por levantarte cada vez que has tropezado con m&aacute;s fuerza, por seguir sin importar el no de los dem&aacute;s porque sabias que era lo correcto y por cada acci&oacute;n con la que haces este mundo un lugar mejor sin esperar nada a cambio.<br>
                 Por eso y m&aacute;s quiero en este d&iacute;a felicitarte y desearte el mejor de los d&iacute;as, record&aacute;ndote que eres una mujer sin igual, que llenas de amor, alegr&iacute;a y felicidad mi vida. Sigue luchando por tus sue&ntilde;os y por hacer del mundo un mejor lugar.<br><br>
@@ -33,9 +33,11 @@ and open the template in the editor.
                 Te Amo.
 </label>
         </div>
-        
-        <div class="mainBox">Un presente para ti</div>
-    <center><div class="img"><img src="img/imgBoleta.png"></div></center>
+        <br>
+        <div class="mainBox">Un presente para ti
+        <center><div class="img"><img src="img/imgBoleta.png"></div></center>
+        </div>
+    
         
     </body>
 </html>
